68d665f70628867a758b1a904fde3388a3e3e50e
Oliver <olipro@uptheinter.net>
2020-11-24 15:17:24 +0000

    Add DestinyRP config.

---
+++ apicheck/config_apicheck.lua
@@ -0,0 +1,22 @@
+--[[
+    Sonoran Plugins
+
+    Plugin Configuration
+
+    Put all needed configuration in this file.
+]]
+local config = {
+    enabled = true,
+    pluginName = "apicheck", -- name your plugin here
+    pluginAuthor = "SonoranCAD", -- author
+    requiresPlugins = {}, -- required plugins for this plugin to work, separated by commas
+
+    -- put your configuration options below
+    myConfigOption = "value"
+}
+
+-- IMPORTANT: UNCOMMENT THE BELOW LINE ON ACTUAL PLUGINS!
+
+if config.enabled then
+    Config.RegisterPluginConfig(config.pluginName, config)
+end
